f72bb94b410ad1e0f8ee394feb515b9aba8519a2
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-17 20:58:10 -0700

    Comando print programando en C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - despliega el mensaje con la funcion printf
+ *
+ *Return: 0
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
