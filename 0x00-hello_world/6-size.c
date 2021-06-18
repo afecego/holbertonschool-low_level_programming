5ec3309921447d7b696aa72220f6b9178db2fa5f
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-17 21:59:03 -0700

    Programa en C para tamaño del computador

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int intType;
 long int longType;
 long long int longlongType;
 float floatType;
-printf("Size of a char: %zu byte (s)\n", sizeof(charType));
-printf("Size of an int: %zu byte (s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte (s)\n", sizeof(longType));
-printf("Size of a long long int: %zu byte (s)\n", sizeof(longlongType));
-printf("Size of a float: %zu byte (s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
