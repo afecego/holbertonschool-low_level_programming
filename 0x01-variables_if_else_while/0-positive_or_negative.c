736f9191529b605990479ca4ada366f7e327e733
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-18 16:38:55 -0700

    Indicacion de numero positivo o negativo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,31 @@
+#include <stddio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - notifica si numero es positivo o negativo
+ *
+ * Return: (0)
+ */
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-  /* your code goes there */
+
+/* your code goes there */
+
+if (n > 0)
+{
+printf(" is positive\n");
+}
+if (n == 0)
+{
+printf(" is zero\n");
+}
+else
+{
+printf(" is negative\n");
+}
 return (0);
 }
