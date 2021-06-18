f1b08e5bfca094fd6cfa02fbfc6709217461a57e
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-17 19:01:39 -0700

    Mi primer codigo C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - despliega el mensaje con la funcion puts
+ *
+ *Return: 0
+ *
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
