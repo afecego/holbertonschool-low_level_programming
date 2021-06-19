b022d4180a2aa12ef6afa3ff4d970ba3de0bee1b
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-18 22:00:43 -0700

    Abecedario en mayuscula

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print del abecedario
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+
+char abc;
+for (abc = 'a' ; abc <= 'z' ; abc++)
+
+{
+putchar(abc);
+}
+for (abc = 'A' ; abc <= 'Z' ; abc++)
+{
+putchar(abc);
+}
+putchar('\n');
+return (0);
+}
