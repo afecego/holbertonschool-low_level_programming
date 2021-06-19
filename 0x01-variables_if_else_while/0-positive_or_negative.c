3cd9e0ad92d38a35870edc3eb78574d405575293
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-18 17:15:49 -0700

    Numeros positivos y negativos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf(" is positive\n");
+printf("%d is positive\n", n);
 }
-if (n == 0)
+else if (n == 0)
 {
-printf(" is zero\n");
+printf("%d is zero\n", n);
 }
 else
 {
-printf(" is negative\n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
