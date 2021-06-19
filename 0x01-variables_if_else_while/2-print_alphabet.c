42a772a0e6bfe8d877147322181b478ccb8aef0b
Adrian Felipe Ceron <afecego@gmail.com>
2021-06-18 21:38:33 -0700

    Abecedario

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print del abecedario
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+
+char abc;
+for (abc = 'a' ; abc <= 'z' ; abc++)
+
+{
+putchar(abc);
+}
+putchar('\n');
+return (0);
+}
