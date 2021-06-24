47a70e8c4ba8f8bd694190c6c5306ccfb7c9da57
afecego <afecego@gmail.com>
2021-06-23 19:57:43 -0700

    Operacion entre dos enteros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - check the code for Holberton School students.
+ * @i: Parametro uno
+ * @j: Parametro dos
+ * Return: Always 0.
+ */
+
+int add(int i, int j)
+{
+int resul;
+
+resul = i + j;
+
+return (resul);
+}
