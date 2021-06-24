e0ec122bb052660fa96952f55d803008dea6eb20
afecego <afecego@gmail.com>
2021-06-24 16:48:02 -0700

    Identificacion de digito

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code for Holberton School students.
+ *
+ * @c: Parametro para la funcion
+ *
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
