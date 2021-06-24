48787866ad6dc57956e255d7343138293c24d265
afecego <afecego@gmail.com>
2021-06-23 17:09:05 -0700

    Valor absoluto

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code for Holberton School students.
+ * @i: Parameter of the funtion
+ * Return: Always 0.
+ */
+
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (-i);
+}
+}
