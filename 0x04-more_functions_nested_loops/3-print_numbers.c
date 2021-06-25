4ae59631383571aafbf5de5b9e45e186b7b392e5
afecego <afecego@gmail.com>
2021-06-24 20:38:54 -0700

    Impresion de numeros

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int n;
+for (n = '0' ; n <= '9' ; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
