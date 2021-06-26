895a584773d546801b6653bed1d297c626053ae5
afecego <afecego@gmail.com>
2021-06-25 19:55:06 -0700

    Triangulo de numerales

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - check the code for Holberton School students.
+ * @size: Parametro necesario
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+int x, y;
+
+for (x = 1 ; x <= size ; x++)
+{
+for (y = 1 ; y <= size ; y++)
+{
+if (y <= size - x)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+if (size < 1)
+{
+_putchar('\n');
+}
+}
+}
