3a15d31316c70bac6925bb37a11807fc8a729590
afecego <afecego@gmail.com>
2021-06-25 19:18:39 -0700

    Numeros hasta el 100 cambiando multiplos

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+for (n = 1 ; n <= 100 ; n++)
+{
+if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", n);
+}
+}
+printf("\n");
+return (0);
+}
