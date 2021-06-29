1f1e0bbd273fe6a55223ce8616e0c5e45e4b44ab
afecego <afecego@gmail.com>
2021-06-28 22:25:54 -0700

    Archivos de funciones

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: Parametro a imprimir
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+*n = 98;
+}
