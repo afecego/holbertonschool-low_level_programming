25e866e3ff467b6973adb575390e4a6cdeca53c3
afecego <afecego@gmail.com>
2021-06-29 17:26:41 -0700

    Impresion frase

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts - check the code for Holberton School students.
+ * @str: Parametro necesarios en la funcion
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+int i;
+
+i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
