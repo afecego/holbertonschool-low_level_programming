6334d812e0e2fc972b84614d53ee105bead4e3ae
afecego <afecego@gmail.com>
2021-06-29 19:31:58 -0700

    Frase al reves

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - check the code for Holberton School students.
+ * @s: Parametro necesario para la funcion
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int i;
+int cont = 0;
+for (i = 0 ; *(s + 1) != '\0' ; i++)
+{
+cont++;
+}
+for (i = cont - 1 ; i >= 0 ; i--)
+{
+_putchar(*(s + i));
+}
+_putchar('\n');
+}
