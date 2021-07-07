6bfba4d4ce23f04ab4b96898524fb580bb90c157
afecego <afecego@gmail.com>
2021-07-07 16:53:39 -0700

    Imprimir palabra con recursividad

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-int i;
 
-i = 0;
-if (s[i] != '\0')
+if (*s != '\0')
 {
-_putchar(s[i]);
-_puts_recursion(&s[i + 1]);
+_putchar(*s);
+_puts_recursion(s + 1);
+return;
 }
 else
-_putchar('\n');
+_putchar(10);
 }
