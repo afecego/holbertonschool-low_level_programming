c611608b4586886d464bed9ab03e1ca864402393
afecego <afecego@gmail.com>
2021-07-06 21:15:06 -0700

    Set value of a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: Parametro necesario para la funcion
+ * @to: Parametro necesario para la funcion
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
