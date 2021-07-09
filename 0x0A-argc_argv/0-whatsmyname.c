0eac60936dae53b159bf2a4bce57b91ab8dd02ec
afecego <afecego@gmail.com>
2021-07-09 16:18:52 -0700

    Imprimir programa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Print the name of programs
+ * @argc: cuenta los argumentos
+ * @argv: muestra lo que contiene el puntero en cada posicion
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
