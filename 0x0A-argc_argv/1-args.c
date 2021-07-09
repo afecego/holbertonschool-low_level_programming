5928004e60d90a1cde0c1c21856a51c0d52b27c0
afecego <afecego@gmail.com>
2021-07-09 16:34:28 -0700

    Imprimir cantidad de programas ejecutados

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Print the name of programs
+ * @argc: cuenta los argumentos
+ * @argv: muestra lo que contiene el puntero en cada posicion
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
