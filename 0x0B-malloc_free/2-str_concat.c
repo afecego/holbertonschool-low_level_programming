5e7936deb27bba6061a9ec0c3bb9c234c98876c2
afecego <afecego@gmail.com>
2021-07-14 16:30:08 -0700

    Concatenar

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,25 +13,38 @@ char *str_concat(char *s1, char *s2)
 int x, y, u;
 char *con;
 
+if (s1 == NULL)
+{
+x = 0;
+}
+else
+{
 for (x = 0 ; x < s1[x] ; x++)
 ;
+}
+if (s2 == NULL)
+{
+y = 0;
+}
+else
+{
 for (y = 0 ; y < s2[y] ; y++)
 ;
-
+}
 con = malloc((x + y + 1) * sizeof(char));
 
 if (con == NULL)
-
+{
 return (NULL);
-
+}
 for (u = 0; u < x; u++)
-
+{
 con[u] = s1[u];
-
+}
 for (u = 0; u < y; u++)
-
+{
 con[u + x] = s2[u];
-
+}
 con[x + y] = '\0';
 return (con);
 }
