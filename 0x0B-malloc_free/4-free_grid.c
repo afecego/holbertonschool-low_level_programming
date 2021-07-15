35b56e808ad47685be0520afe81cc81367fd7d04
afecego <afecego@gmail.com>
2021-07-14 21:09:58 -0700

    Liberacion de memoria

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+
+free(grid[i]);
+
+free(grid);
+}
