9cd8ff34bdc74754fdd81a07e494c44ef40cb82b
afecego <afecego@gmail.com>
2021-07-19 21:51:21 -0700

    Liberacion de espacio

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Liberar espacio
+ * @d: Parametro de entrada
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->age);
+free(d->owner);
+
+}
