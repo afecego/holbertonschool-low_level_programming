4d1705b68ad7eeb5467947d09daa9d8548309fed
afecego <afecego@gmail.com>
2021-07-19 18:25:34 -0700

    Definicion variables

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCTUR
+#define STRUCTUR
+#include <stdio.h>
+/**
+ * struct dog - Nombre de la estructura
+ * @name: Nombre del dog
+ * @age: Edad del dog
+ * @owner: Dueño del dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
