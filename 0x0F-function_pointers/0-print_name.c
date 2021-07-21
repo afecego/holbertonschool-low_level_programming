5df16a0880dc5c75071131b082844f3d6e696823
afecego <afecego@gmail.com>
2021-07-21 14:57:53 -0700

    Impresion de nombre

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: Puntero que apunta a la funcion
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
