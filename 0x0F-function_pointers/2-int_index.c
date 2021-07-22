2a923781aa04a9b8a36ce44263fe011e00f67002
afecego <afecego@gmail.com>
2021-07-21 17:04:51 -0700

    Busqueda de un entero en array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is equal to 98
+ * @array: parametro de entrada del array
+ * @size: Tamaño del array
+ * @cmp: Puntero de la funcion
+ * Return: 0 if false, something else otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (size <= 0)
+{
+return (-1);
+}
+if (array == NULL && cmp == NULL)
+{
+return (-1);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+if ((*cmp)(array[i]) != 0)
+
+return (i);
+}
+}
+return (-1);
+}
