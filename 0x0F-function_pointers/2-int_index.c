1e66a1a48cbf3be83ce50c2fb86bc58771ab7f38
afecego <afecego@gmail.com>
2021-07-21 17:24:50 -0700

    Busqueda de un entero en array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,19 +13,21 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
 
-if (size <= 0)
-{
-return (-1);
-}
 if (array == NULL && cmp == NULL)
 {
 return (-1);
 }
-
+if (size <= 0)
+{
+return (-1);
+}
+else
+{
 for (i = 0; i < size; i++)
 {
 if ((*cmp)(array[i]) != 0)
 return (i);
+}
 }
 return (-1);
 }
