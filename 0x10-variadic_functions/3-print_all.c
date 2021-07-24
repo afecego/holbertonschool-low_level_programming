41e7a6de7ce50f29cc8f14a655caf6b2a4f5d238
afecego <afecego@gmail.com>
2021-07-23 22:35:21 -0700

    Identificacion de type de variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,99 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - Funcion de char..
+ * @c: Variable char
+ * Return: Always 0.
+ */
+
+void print_char(va_list c)
+{
+printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_int - Funcion de integer.
+ * @i: Variable int
+ * Return: Always 0.
+ */
+
+void print_int(va_list i)
+{
+printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_float - Funcion de float.
+ * @f: Variable float
+ * Return: Always 0.
+ */
+
+void print_float(va_list f)
+{
+printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_string - Funcion de string.
+ * @s: Variable string
+ * Return: Always 0.
+ */
+
+void print_string(va_list s)
+{
+char *out = va_arg(s, char *);
+
+if (out == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", out);
+}
+}
+
+/**
+ * print_all - Funcion de seleccion de tipo de string.
+ * @format: String con la informacion del tipo de variable
+ * Return: Always 0.
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list ap;
+int i = 0;
+int j = 0;
+char *separador = "";
+
+impresion valores[] = {
+
+{"c", print_char},
+{"i", print_int},
+{"s", print_string},
+{"f", print_float},
+{NULL, NULL}
+
+};
+
+va_start(ap, format);
+
+while (format && format[i])
+{
+j = 0;
+while (valores[j].t != NULL)
+{
+if (*(valores[j].t) == format[i])
+{
+printf("%s", separador);
+valores[j].f(ap);
+separador = ", ";
+break;
+}
+j++;
+}
+i++;
+}
+printf("\n");
+va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,22 @@
 #ifndef FUNCTIONS_VARIADICS_H
 #define FUNCTIONS_VARIADICS_H
-
-
 #include <stdio.h>
 #include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+typedef struct impre
+{
+char *t;
+void (*f)(va_list);
+
+}impresion;
+void print_char(va_list c);
+void print_int(va_list i);
+void print_float(va_list f);
+void print_string(va_list s);
 
 #endif
