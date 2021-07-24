9d75221ff6e86b9549d1d6e8e0a4974287619721
afecego <afecego@gmail.com>
2021-07-23 18:29:45 -0700

    Suma con variacs funtions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,10 @@
-#ifndef _FUNCTIONS_VARIADICS_
-#define _FUNCTIONS_VARIADICS_
+#ifndef FUNCTIONS_VARIADICS_H
+#define FUNCTIONS_VARIADICS_H
 
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
