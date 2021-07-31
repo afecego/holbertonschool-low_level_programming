aa818f3d7cafb66df835c9b7d4bd8012b3cf4a6e
afecego <afecego@gmail.com>
2021-07-30 20:08:50 -0700

    Count of elementes from de nodo

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - Count elements from nodos
+ * @h: constant
+ * Return: Always 0.
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+
+while (h)
+i++;
+h = h->next;
+
+return (i);
+}
