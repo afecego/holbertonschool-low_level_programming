4b2045f0a2ba7ea14f766a2226d06ed5e184a8f0
afecego <afecego@gmail.com>
2021-08-03 14:23:17 -0700

    Free memory and head equal to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free memory
+ * @head: List
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL && *head == NULL)
+{
+return;
+}
+while ((*head)->next != NULL)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+free((*head));
+*head = NULL;
+
+}
