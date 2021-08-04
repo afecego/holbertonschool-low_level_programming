fc1b58b0a3be38562e18ec4d92ce76d828f83ac7
afecego <afecego@gmail.com>
2021-08-04 15:58:46 -0700

    Insert nodo betwn nodos

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,42 +4,41 @@
  * insert_nodeint_at_index - check the code
  * @head: list
  * @idx: Index de los nodos
- * @n: Dato a ingresar en el Nodo* Return: Always 0.
+ * @n: Dato a ingresar en el Nodo
+ * Return: New nodo
  */
-
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i;
-listint_t *old, *new;
+	unsigned int i;
+	listint_t *old, *new;
 
-if (head == NULL || *head == NULL)
-return (NULL);
-if (idx != 0)
-{
-old = *head;
-for (i = 0; i < idx - 1 && old != NULL; i++)
-{
-old = old->next;
-}
-if (old == NULL)
-{
-return (NULL);
-}
-}
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-{
-return (NULL);
-}
-new->n = n;
-if (idx == 0)
-{
-new->next = *head;
-*head = new;
-return (new);
-}
-new->next = old->next;
-old->next = new;
-return (new);
+	if (head == NULL)
+		return (NULL);
+	if (idx != 0)
+	{
+		old = *head;
+		for (i = 0; i < idx - 1 && old != NULL; i++)
+		{
+			old = old->next;
+		}
+		if (old == NULL)
+		{
+			return (NULL);
+		}
+	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = old->next;
+	old->next = new;
+	return (new);
 }
