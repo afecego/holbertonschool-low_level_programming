aa76c5a4bde2766e84c0959096b7dfe35b4d95bc
afecego <afecego@gmail.com>
2021-08-03 21:24:15 -0700

    Insert nodo in a midle

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - check the code
+ * @head: list
+ * @idx:
+ * @n:
+ * Return: Always 0.
+ */
+
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+unsigned int i;
+listint_t *old, *new;
+
+if (head == NULL || *head == NULL)
+return (NULL);
+if (idx != 0)
+{
+old = *head;
+for (i = 0; i < idx - 1 && old != NULL; i++)
+{
+old = old->next;
+}
+if (old == NULL)
+{
+return (NULL);
+}
+}
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n;
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+new->next = old->next;
+old->next = new;
+return (new);
+}
