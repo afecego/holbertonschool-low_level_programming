7de8277f0700b31fd9bafba8544bd67de9b43e6e
afecego <afecego@gmail.com>
2021-08-05 22:09:08 -0500

    Get bit by index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - print representation from binary number
+ * @n: long of bit
+ * @index: index del bit
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+    unsigned int x, y;
+    if (index > sizeof(unsigned int) * 8)
+    {
+        return (-1);
+    }
+    x = n >> index;
+    y = x & 1;
+
+    return (y);
+}
