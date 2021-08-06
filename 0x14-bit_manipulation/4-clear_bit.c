5cc64cd477263d7ef06b82c60b48433529d22a14
afecego <afecego@gmail.com>
2021-08-06 00:23:36 -0500

    Set value 0 in index define

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,24 +2,24 @@
 /**
  * clear_bit - clear bit whit cero
  * @n: long of bit
- * @index: index del bit 
+ * @index: index del bit
  * Return: Always 0.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned int bit;
+	unsigned int bit;
 
-    if (n == NULL)
-    {
-        return (-1);
-    }
-    if (index > sizeof(unsigned int) * 8)
-    {
-        return (-1);
-    }
-    bit = 1;
+	if (n == NULL)
+	{
+		return (-1);
+	}
+	if (index > sizeof(unsigned int) * 8)
+	{
+		return (-1);
+	}
+	bit = 1;
 	bit = bit << index;
-    bit = ~bit;
+	bit = ~bit;
 	*n = *n & bit;
 
 	return (1);
