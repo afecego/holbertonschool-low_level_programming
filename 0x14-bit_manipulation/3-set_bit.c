2cba755820981a795d2be6b45c5562619a4ef156
afecego <afecego@gmail.com>
2021-08-05 23:26:36 -0500

    Change bit for 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * get_bit - print representation from binary number
+ * @n: long of bit
+ * @index: index del bit
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bit;
+
+	if (n == NULL)
+	{
+		return (-1);
+	}
+
+	if (index > sizeof(unsigned int) * 8)
+	{
+		return (-1);
+	}
+
+	bit = 1;
+	bit = bit << index;
+	*n = *n | bit;
+
+	return (1);
+
+}
