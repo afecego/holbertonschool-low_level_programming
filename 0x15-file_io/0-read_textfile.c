32128c903da44a89e9c3780dd8c1d38f3fd83f1d
afecego <afecego@gmail.com>
2021-08-09 18:35:56 -0500

    read and print a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - read file and print
+ * @filename: File
+ * @letters: Number of letter to read and print
+ * Return: Number of letters.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t bytes;
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (filename == NULL || buf == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	bytes = read(fd, buf, letters);
+	write(STDIN_FILENO, buf, bytes);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (bytes);
+}
