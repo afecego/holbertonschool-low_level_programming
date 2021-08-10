5d40761fdf03e824a25a26a6ce78ae41093b539c
afecego <afecego@gmail.com>
2021-08-09 22:10:41 -0500

    Add append in file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - check the code
+ * @filename: Name of file to create
+ * @text_content: Text in the file
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, on;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	on = open(filename, O_RDWR | O_APPEND);
+
+	if (on == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			;
+		}
+		write(on, text_content, i);
+		
+	}
+	close(on);
+	return (1);
+}
