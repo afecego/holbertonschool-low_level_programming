1a7b78f5a19545698d20fd126bff8d6bd3c42ff6
afecego <afecego@gmail.com>
2021-08-09 20:28:37 -0500

    create file and return 1 on success

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: Name of file to create
+ * @text_content: Text in the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int on, i;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	on = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (on == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			;
+		}
+		write(on, text_content, i);
+	}
+
+	close(on);
+	return (1);
+
+}
