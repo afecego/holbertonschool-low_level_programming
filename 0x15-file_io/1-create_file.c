8132874fca4d8bdc96bde5d637c1bf949472e244
afecego <afecego@gmail.com>
2021-08-09 20:30:07 -0500

    create file and return 1 on success

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 		{
