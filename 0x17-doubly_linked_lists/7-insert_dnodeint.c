5b751ddf08c751370d22d45abba2e9412d866420
afecego <afecego@gmail.com>
2021-09-17 20:12:48 -0500

    Insert new dode in n position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,36 +10,72 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *old, *new;
+	dlistint_t *current = NULL, *new_node = NULL;
+	unsigned int i = 0, length = 0;
 
 	if (h == NULL)
 		return (NULL);
-	if (idx != 0)
-	{
-		old = *h;
-		for (i = 0; i < idx - 1 && old != NULL; i++)
-		{
-			old = old->next;
-		}
-		if (old == NULL)
-		{
-			return (NULL);
-		}
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
+
+	if (*h == NULL && idx == 0)
+		return (add_dnodeint(h, n));
+
+	length = dlistint_len(*h);
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+	else if (length == idx)
+		return (add_dnodeint_end(h, n));
+
+	current = *h;
+	while (current != NULL)
 	{
-		new->next = *h;
-		*h = new;
-		return (new);
+		if (i == idx)
+		{
+			new_node = createNode(n, current, current->prev);
+			current->prev = new_node;
+			current = new_node;
+			current->prev->next = new_node;
+			return (new_node);
+		}
+		current = current->next;
+		++i;
 	}
-	new->next = old->next;
-	old->next = new;
-	return (new);
+	return (current);
+}
+
+/**
+ * dlistint_len - Print total elements of structur
+ * @h: pointer of head
+ * Return: number of nodes.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int length;
+
+	for (length = 0; h != NULL; length++)
+	{
+		h = h->next;
+	}
+	return (length);
+}
+
+/**
+ * createNode - Creat new node
+ * @n: Date of de new node
+ * @next: Next of de new node
+ * @prev: Previos of the new node
+ * Return: new node.
+ */
+
+dlistint_t *createNode(unsigned int n, void *next, void *prev)
+{
+	dlistint_t *Newnodo = malloc(sizeof(dlistint_t));
+
+	if (Newnodo == NULL)
+		return (NULL);
+
+	Newnodo->n = n;
+	Newnodo->next = next;
+	Newnodo->prev = prev;
+	return (Newnodo);
 }
