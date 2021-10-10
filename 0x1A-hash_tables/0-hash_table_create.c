5d9180db327c4d4858c18b39407c510529bd8b2a
afecego <afecego@gmail.com>
2021-10-10 11:33:48 -0500

    Correction zero

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
 	{
