0d105d303be26c3e5b323d4a89dde834100053d6
afecego <afecego@gmail.com>
2022-01-17 18:12:31 -0500

    Project search algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+# include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: first element of the array to search in
+ * @size: size of the array
+ * @value: the value to search
+ * Return: Always 0.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf ("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_SEARCH
+#define HOLBERTON_SEARCH
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
